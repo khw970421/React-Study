a9ddd271a2262047887476c0faab2a06727bf209
khw970421 <khw970421@kakao.com>
2022-12-29 17:53:34 +0900

    feat: redux BtnCount 기능추가

---
+++ src/components/BtnCount/Redux/App.tsx
@@ -0,0 +1,50 @@
+import Counter from "./components/Counter";
+import { useDispatch, useSelector } from "react-redux";
+import { totalIncreaseCount, totalDecreaseCount } from "./redux/action";
+import { increaseCount, decreaseCount } from "./redux/action";
+import { Container, Title } from "../../../pages/BtnCountPage";
+
+function Redux() {
+  const dispatch = useDispatch();
+  const { first, second, third, totalState } = useSelector((state) => ({
+    totalState: state.totalCountReducer,
+    first: state.countReducer.first,
+    second: state.countReducer.second,
+    third: state.countReducer.third,
+  }));
+
+  console.log(first, second, third);
+
+  const handleIncrease = (val, key) => {
+    dispatch(increaseCount(val, key));
+    dispatch(totalIncreaseCount(totalState));
+  };
+
+  const handleDecrease = (val, key) => {
+    dispatch(decreaseCount(val, key));
+    dispatch(totalDecreaseCount(totalState));
+  };
+
+  return (
+    <Container>
+      <Title>Redux - TotalCount : {totalState}</Title>
+      <Counter
+        onIncrease={() => handleIncrease(first, "first")}
+        onDecrease={() => handleDecrease(first, "first")}
+        val={first}
+      ></Counter>
+      <Counter
+        onIncrease={() => handleIncrease(second, "second")}
+        onDecrease={() => handleDecrease(second, "second")}
+        val={second}
+      ></Counter>
+      <Counter
+        onIncrease={() => handleIncrease(third, "third")}
+        onDecrease={() => handleDecrease(third, "third")}
+        val={third}
+      ></Counter>
+    </Container>
+  );
+}
+
+export default Redux;
---
+++ src/components/BtnCount/Redux/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const index = () => {
+  return (
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  );
+};
+
+export default index;
