100763d0c7e092c3336e96769937d39cbfb0a502
khw970421 <khw970421@kakao.com>
2022-12-29 17:54:44 +0900

    chore: redux BtnCount 추가

--- src/pages/BtnCountPage.tsx
+++ src/pages/BtnCountPage.tsx
@@ -1,9 +1,32 @@
 import ReactCounter from "../components/BtnCount/React";
+import ReduxCounter from "../components/BtnCount/Redux";
+import styled from "styled-components";
 
 export default function BtnCountPage() {
   return (
-    <>
+    <BtnCountPageContainer>
       <ReactCounter />
-    </>
+      <ReduxCounter />
+    </BtnCountPageContainer>
   );
 }
+
+const BtnCountPageContainer = styled.div`
+  display: flex;
+`;
+
+export const Container = styled.div`
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 30px;
+  padding: 30px;
+  background-color: skyblue;
+`;
+
+export const Title = styled.div`
+  padding-bottom: 20px;
+`;
+
+export const Btn = styled.button`
+  margin: 20px 5px;
+`;
